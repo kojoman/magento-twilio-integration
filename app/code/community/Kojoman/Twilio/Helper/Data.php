5f39d036edd7ba95a6c50e23e089016877f17780
kojoman <juantuah@gmail.com>
2014-01-30 21:38:43 -0500

    Add helper methods to get Twilio api key and secret

--- app/code/community/Kojoman/Twilio/Helper/Data.php
+++ app/code/community/Kojoman/Twilio/Helper/Data.php
@@ -1,6 +1,48 @@
 <?php
 
-class Kojoman_Twilio_Helper_Data extends Mage_Core_Helper_Abstract
+class Kojoman_magento_twilio_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	
+	const XML_PATH_ENABLED						= 'magento_twilio/api/enabled';
+	const XML_PATH_ACCOUNT_SID					= 'magento_twilio/api/key';
+	const XML_PATH_AUTH_TOKEN					= 'magento_twilio/api/secret';
+
+	/** 
+	 * Check to see if extension is enabled
+	 * 
+	 * @return bool 
+	 */
+	public function isEnabled($store = null) 
+	{
+		return Mage::getStoreConfig(self::XML_PATH_ENABLED, $store); 
+	}
+
+	/**
+	 * Get Twilio Account SID
+	 *
+	 * @return string
+	 */ 
+	public function getAccountSID($store = null)
+	{
+		return(Mage::getStoreConfig(self::XML_PATH_ACCOUNT_SID, $store));
+	}
+
+	/**
+	 * Get Twilio Auth Token
+	 *
+	 * @return String
+	 */
+	public function getAuthToken($store = null) 
+	{
+		return(Mage::getStoreConfig(self::XML_PATH_AUTH_TOKEN, $store)); 
+	}
+
+	/**
+	 * Quick and dirty way to debug Magento objects
+	 */
+	public function debug($object)
+	{
+		echo '<pre>';
+		print_r($object);
+		echo '</pre>';
+	}
 }
