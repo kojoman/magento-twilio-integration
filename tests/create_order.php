68364c5643cae7691adbd764376fbfa52f44d281
kojoman <juantuah@gmail.com>
2014-02-15 21:33:42 -0500

    Still have a bug with order not getting created due to invalid shipping method.

--- tests/create_order.php
+++ tests/create_order.php
@@ -31,136 +31,136 @@ $customer->setStoreId(Mage::app()->getStore()->getId());
 $customer->loadByEmail($email);
 
 //Delete customer
-if($customer->getId()) {
-	$customerId = $customer->getId();
-	//$customer = Mage::getModel('customer/customer')->loadById($customerId);
+// if($customer->getId()) {
+// 	$customerId = $customer->getId();
+// 	//$customer = Mage::getModel('customer/customer')->loadById($customerId);
 
-	Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
-	try {
-		$customer->delete();
-		echo "Customer deleted.\n";
-	} catch (Exception $e) {
-		Zend_Debug::dump($e->getMessage());
-	}
-}
+// 	Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+// 	try {
+// 		$customer->delete();
+// 		echo "Customer deleted.\n";
+// 	} catch (Exception $e) {
+// 		Zend_Debug::dump($e->getMessage());
+// 	}
+// }
 
 //Create new customer if customer with specified email does not exist. 
 if(!$customer->getId()) {
 	echo "Customer does not exist.\n";
 	$customer->setEmail($email);
-	$customer->setFirstName('FirstName');
-	$customer->setLastName('LastName');
+	$customer->setFirstName('FirstName');  	//TODO: Customer First Name not appearing in admin
+	$customer->setLastName('LastName'); 	//TODO: Customer Last Name not appearing in admin
 	$customer->setPassword($password);
-
-	Zend_Debug::dump($customer);
 
 	// Save new customer
 	try {
 		$customer->save();
 		$customer->setConfirmation(null);
 		$customer->save();
-		$customer->sendNewAccountEmail();
-		echo "New customer saved";
-		Zend_Debug::dump($customer);
-
+		//$customer->sendNewAccountEmail();
+		//Mage::Log($customer->getName)
 	} catch (Exception $e) {
 		Zend_Debug::dump($e->getMessage());
+		Mage::logException($e->getMessage());
 	}
 }
 
-// // Login customer
-// $x = Mage::getSingleton('customer/session')->loginById($customer->getId());
-// Zend_Debug::dump($x);
+// Login customer
+Mage::getSingleton('customer/session')->loginById($customer->getId());
 
 
-// // Create shipping and billing address for customer. Needed for checkout 
-// $address = array(
-// 	'firstname' => 'FirstName',
-// 	'lastname'  => 'LastName', 
-// 	'street'	=> array (
-// 		'0' => 'Street Address part 1',
-// 		'1' => 'Street Address part 2',
-// 	),
-// 	'city'		=> 'New York',
-// 	'region_id' => '15',
-// 	'region' 	=> 'NY',
-// 	'postcode' 	=> '10000',
-// 	'country_id' => 'US', 
-// 	'telephone'	=> '1-234-567-8912', 
-// );
+// Create shipping and billing address for customer. Needed for checkout 
+$address = array(
+	'firstname' => 'FirstName',
+	'lastname'  => 'LastName', 
+	'street'	=> array (
+		'0' => 'Street Address part 1',
+		'1' => 'Street Address part 2',
+	),
+	'city'		=> 'New York',
+	'region_id' => '15',
+	'region' 	=> 'NY',
+	'postcode' 	=> '10000',
+	'country_id' => 'US', 
+	'telephone'	=> '1-234-567-8912', 
+);
 
-// $customerAddress = Mage::getModel('customer/address');
+$customerAddress = Mage::getModel('customer/address');
 
-// echo "Customer Address object created.\n";
-// Zend_Debug::dump($customerAddress);
+$customerAddress->setData($address)
+				->setCustomerId($customer->getId())
+				->setIsDefaultBilling('1')
+				->setIsDefaultShipping('1')
+				->setSaveInAddressBook('1');
 
-// $customerAddress->setData($address)
-// 				->setCustomerId($customer->getId())
-// 				->setIsDefaultBilling('1')
-// 				->setIsDefaultShipping('1')
-// 				->setSaveInAddressBook('1');
+try {
+	$customerAddress->save();
+} catch (Exception $e) {
+	Mage::logException($e->getMessage());
+	Zend_Debug::dump($e->getMessage());
+}
 
-// try {
-// 	$customerAddress->save();
-// } catch (Exception $e) {
-// 	Zend_Debug::dump($e->getMessage());
-// }
+$x = Mage::getSingleton('checkout/session')->getQuote()->setBillingAddress(Mage::getSingleton('sales/quote_address')->importCustomerAddress($customerAddress));
 
-// echo "Customer Address object updated.\n";
-// Zend_Debug::dump($customerAddress);
+//Zend_Debug::dump($x);
 
-// Mage::getSingleton('checkout/session')->getQuote()->setBillingAddress(Mage::getSingleton('sales/quote_address')->importCustomerAddress($customerAddress));
+// We now have a logged in customer
+// Next step is to add products to his/her cart
+// 1112 is the sku for 'Chair' if you use Magento's default data set
+$sku = '1112';
 
-// // We now have a logged in customer
-// // Next step is to add products to his/her cart
-// $sku = '1112';
+$product = Mage::getModel('catalog/product')->getCollection()
+			->addAttributeToFilter('sku', $sku)
+			->addAttributeToSelect('*')
+			->getFirstItem();
 
-// $product = Mage::getModel('catalog/product')->getCollection()
-// 			->addAttributeToFilter('sku', $sku)
-// 			->addAttributeToSelect('*')
-// 			->getFirstItem();
+echo "product object has been created.\n";
+Zend_Debug::dump($product);
 
-// echo "product object has been created.\n";
-// Zend_Debug::dump($product);
+// Load full product
+$product->load($product->getId());
+$cart = Mage::getSingleton('checkout/cart');
+//$cart = Mage::getModel('sales/quote')->loadByCustomer($customer->getId());
+// Clear cart before adding products
+$cart->truncate();
+$cart->save();
+$cart->getItems()->clear()->save();
 
-// // Load full product
-// $product->load($product->getId());
-// $cart = Mage::getSingleton('checkout/cart');
-// // Clear cart before adding products
-// $cart->truncate();
-// $cart->save();
-// $cart->getItems()->clear()->save();
+try {
+	$cart->init();
+	$cart->addProduct($product);
+	$cart->save();
+	Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
+	echo "Product added to cart\n";
+} catch (Exception $e) {
+	Mage::logException($e->getMessage());
+	Zend_Debug::dump($e->getMessage());
+}
+//unset($product);
 
-// try {
-// 	$cart->addProduct($product);
-// 	$cart->save();
-// } catch (Exception $e) {
-// 	Zend_Debug::dump($e->getMessage());
-// }
-// unset($product);
+// Now let's make the purchase 
+$storeId = Mage::app()->getStore()->getId();
+$checkout = Mage::getSingleton('checkout/type_onepage');
+echo "Checkout Object created.\n";
+//Zend_Debug::dump($checkout);
+$checkout->initCheckout();
+$checkout->saveCheckoutMethod('register');
+//$checkout->getQuote()->getShippingAddress()->setShippingMethod('flatrate_flatrate');
+// $checkout->getQuote()->getShippingAddress()->unsGrandTotal(); // clear the values so they don't take part in calculation
+// $checkout->getQuote()->getShippingAddress()->unsBaseGrandTotal();
+// $checkout->getQuote()->getShippingAddress()->setCollectShippingRates(true)->save();
 
-// // Now let's make the purchase 
-// $storeId = Mage::app()->getStore()->getId();
-// $checkout = Mage::getSingleton('checkout/type_onepage');
-// echo "Checkout Object created.\n";
-// //Zend_Debug::dump($checkout);
-// $checkout->initCheckout();
-// $checkout->saveCheckoutMethod('register');
-// //$checkout->getQuote()->getShippingAddress()->setShippingMethod('flatrate_flatrate');
-// // $checkout->getQuote()->getShippingAddress()->unsGrandTotal(); // clear the values so they don't take part in calculation
-// // $checkout->getQuote()->getShippingAddress()->unsBaseGrandTotal();
-// // $checkout->getQuote()->getShippingAddress()->setCollectShippingRates(true)->save();
+//             $checkout->getQuote()->getShippingAddress()->collectTotals();    //collect totals and ensure the initialization of the shipping methods
 
-// //             $checkout->getQuote()->getShippingAddress()->collectTotals();    //collect totals and ensure the initialization of the shipping methods
-
-// //             $checkout->getQuote()->collectTotals();
-// $shippingMethod = $checkout->saveShippingMethod('flatrate_flatrate');
-// $checkout->savePayment(array('method' => 'checkmo'));
-// try {
-// 	$checkout->saveOrder();
-// } catch (Exception $e) {
-// 	Zend_Debug::dump($e->getMessage());
-// }
+//             $checkout->getQuote()->collectTotals();
+$shippingMethod = $checkout->saveShippingMethod('flatrate');
+Zend_Debug::dump($shippingMethod);
+$checkout->savePayment(array('method' => 'checkmo'));
+try {
+	$checkout->saveOrder();
+} catch (Exception $e) {
+	Zend_Debug::dump($e->getMessage());
+}
 
 // //Zend_Debug::dump($checkout);
 
