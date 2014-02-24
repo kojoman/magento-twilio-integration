1ecaf749129198b2e93a4cbd8591320988edb8c5
kojoman <juantuah@gmail.com>
2014-02-24 11:07:50 -0500

    Create installer script

---
+++ app/code/community/Kojoman/Twilio/sql/twilio_setup/mysql-install-0.1.0.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Tables Needed = kojoman_twilio
+ * Columns:
+ *  - message_id
+ *  - log_at
+ *  - to
+ *  - sms_body
+ */
+
+$installer = $this;
+
+Mage::log($installer);
+
+$installer->run("
+    CREATE TABLE `{$installer->getTable('kojoman/twilio')}` (
+      `sms_id` int(11) NOT NULL auto_increment,
+      `sms_to` VARCHAR(255) NOT NULL default '',
+      `sms_body` text,
+      `date` datetime default NULL,
+      `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (`sms_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET = UTF8;
+
+    INSERT INTO `{$installer->getTable('kojoman/twilio')}` VALUES (1, '(123) 456-7890', 'Hello World', '2014-02-24 00:00:00','2014-02-24 23:12:30');
+    ");
+
+$installer->endSetup();
