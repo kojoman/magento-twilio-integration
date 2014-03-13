fe1ad37fac215f38e1d294f6249f0528dcc02599
kojoman <juantuah@gmail.com>
2014-03-12 23:49:40 -0400

    Add Twiml class
    Courtesy of avalance123's magento-twilio extension

---
+++ app/code/community/Kojoman/Twilio/Block/Twiml.php
@@ -0,0 +1,28 @@
+<?php
+
+class Kojoman_Twilio_Block_Twiml extends Mage_Core_Block_Template
+{
+    protected $phoneNumber;
+    protected $callerId;
+
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+        return $this;
+    }
+
+    public function setCallerId($callerId)
+    {
+        $this->callerId = $callerId;
+    }
+
+    public function getCallerIdString()
+    {
+        return ' callerId="' . $this->callerId . '"';
+    }
+
+    public function getRecepient()
+    {
+        return "<Number>" . $this->phoneNumber . "</Number>";
+    }
+}
